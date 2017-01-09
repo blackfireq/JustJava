12de56bf71a3eadbe6baa43a83124f486e1ed500
Mikem <Mikemangan00@gmail.com>
2017-01-09 15:16:32 -0500

    final state
    this is the end of the lesson 2B for score keeper

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -21,8 +21,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity * price);
+        int totalPrice = quantity * price;
+        String priceMessage = "Total: $" + totalPrice + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -39,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     public void increment(View view) {
